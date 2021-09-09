eac31cfb2b8f00b5a62ccfc93eebd7a6c4570e90
NatiPerez459 <natalia.roldan@upr.edu>
2021-09-09 13:08:49 -0400

    Added void printable function and bool variable print

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main () {
 
-    string name;
-     
-     cout << "What is your name?" << endl;
-     cin >> name;
+}
 
-     cout<< "Welcome, " << name << endl; 
+/*Verify that the parameter given by the function is printable. If
+it is printable, check if it is digit and print, ”It is printable and digit.”. If it is
+not a digit, print ”It is printable”. If it’s not printable or a digit, print ”It is
+not printable”*/
 
-
-    return 0;
+void printable(char character) {
+    bool print = isprint(character);
 }
